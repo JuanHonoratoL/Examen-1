8fc1f9288ed64bf031524fdb3e5550b95741c42d
JP <juanbloou@gmail.com>
2025-03-18 23:23:39 -0600

    Avance

--- src/main/java/UPIIZ/Examen/I/Controllers/LoginController.java
+++ src/main/java/UPIIZ/Examen/I/Controllers/LoginController.java
@@ -1,4 +1,13 @@
 package UPIIZ.Examen.I.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+
+    @GetMapping
+    public String Login() {
+        return "login";
+    }
 }
--- src/main/java/UPIIZ/Examen/I/Controllers/ReservaHotel.java
+++ src/main/java/UPIIZ/Examen/I/Controllers/ReservaHotel.java
@@ -1,4 +1,10 @@
 package UPIIZ.Examen.I.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ReservaHotel {
+
+
 }
